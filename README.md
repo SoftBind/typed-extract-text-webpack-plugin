27a63cff4507d5160dc65939a56d1daf74b951f1
Dawid 'nCore' Opis <dawid.opis@thetradenet.com>
2016-04-01 23:17:09 +0200

    * new: Finished typed definition for extract-text-webpack-plugin

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 The type definition for [`https://github.com/webpack/extract-text-webpack-plugin`](https://github.com/webpack/extract-text-webpack-plugin)
 
-# LICENSE
+## Developed at:
+    //   ____        ____       ____             _
+    //  / ___| ___  / ___| ___ | __ ) _   ___  _(_)
+    // | |  _ / _ \| |  _ / _ \|  _ \| | | \ \/ / |
+    // | |_| | (_) | |_| | (_) | |_) | |_| |>  <| |
+    //  \____|\___/ \____|\___/|____/ \__,_/_/\_\_|
+    //
+
+## LICENSE
 
 MIT
--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,36 @@
+// Copyright (c) 2016
+//   ____        ____       ____             _
+//  / ___| ___  / ___| ___ | __ ) _   ___  _(_)
+// | |  _ / _ \| |  _ / _ \|  _ \| | | \ \/ / |
+// | |_| | (_) | |_| | (_) | |_) | |_| |>  <| |
+//  \____|\___/ \____|\___/|____/ \__,_/_/\_\_|
+//
+// Licensed under MIT.
+
 declare module 'extract-text-webpack-plugin' {
 
-    class ExtractTextPlugin {
-        constructor( id, filename?:any, options?:any );
+    namespace ExtractTextPlugin {
+        interface PluginOptions {
+            // extract from all additional chunks too (by default it extracts only from the initial chunk(s))
+            allChunks?:boolean,
+
+            // disables the plugin
+            disable?:boolean
+        }
+
+        interface LoaderOptions {
+            // override the publicPath setting for this loader.
+            publicPath?:string
+        }
+
+        function extract( loader:string | Array<string>, options?:LoaderOptions );
+        function extract( before:any, loader?:string | Array<string>, options?:LoaderOptions );
     }
 
-    namespace ExtractTextPlugin {
-        function extract( before:any, loader?:any, options?:any );
+    class ExtractTextPlugin {
+        constructor( filename:string, options?:ExtractTextPlugin.PluginOptions );
+        constructor( id:string, filename:string, options:ExtractTextPlugin.PluginOptions );
+        constructor( id?:string, filename?:string, options?:ExtractTextPlugin.PluginOptions );
     }
 
     export = ExtractTextPlugin;
